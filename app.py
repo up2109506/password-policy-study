ac2f07a843d747f0510c43282eef939df4b429b2
up2109506 <128398402+up2109506@users.noreply.github.com>
2026-04-04 17:59:20 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, jsonify
+import csv
+import time
+
+app = Flask(__name__)
+
+@app.route("/")
+def consent():
+    return render_template("consent.html")
+
+@app.route("/test")
+def index():
+    return render_template("index.html")
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    data = request.json
+
+    session_id = data["session_id"]
+    policy_id = data["policy_id"]
+    policy_type = data["policy_type"]
+    time_taken = data["time"]
+    attempts = data["attempts"]
+    length = data["length"]
+    strength = data["strength"]
+
+    with open("data.csv", "a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([session_id, policy_id, policy_type, time_taken, attempts, length, strength])
+
+    return jsonify({"status": "success"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
